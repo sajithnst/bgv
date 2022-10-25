2857e8d4e56147d9436134a6488b553ff087bb71
sajith <sajith@salesfokuz.in>
2022-10-25 20:34:42 +0530

    changes in the test.py

--- ui/pages/signin.vue
+++ ui/pages/signin.vue
@@ -3,5 +3,34 @@
         <v-app-bar>
             <v-btn icon ><v-icon size="32">mdi-home</v-icon></v-btn>
         </v-app-bar>
+        <v-main>
+            <v-container>
+                <v-form>
+                    <v-col>
+                        <v-row>
+                            <v-text-field label="Email ID"></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-text-field label="Password"></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-btn large block color="teal" @click="signin()"> Sign In</v-btn>
+                        </v-row>
+                    </v-col>
+                </v-form>
+            </v-container>
+        </v-main>
     </v-app>
 </template>
+<script>
+export default{
+    name : 'signinpage',
+    data: () =>({
+        signin : {
+            username : '',
+            password : '',
+            
+        }
+    }),
+}
+</script>
