552234e4c1d27db06cbb01f8bebd980eebefca83
sajith <sajith@salesfokuz.in>
2022-10-27 03:07:15 +0530

    user ui design

--- ui/pages/user.vue
+++ ui/pages/user.vue
@@ -1,14 +1,32 @@
 <template>
-    <v-app user>
+      <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    <v-container class="nav-pic">
+        <v-avatar size="130">
+          <v-icon size="100">mdi-account-circle</v-icon>
+        </v-avatar>
+      </v-container>
+      <v-divider></v-divider>
+      <v-container class="nav-pic">
+        <h2 class="text-center">{{name}}</h2>
+    </v-container>
+    <v-btn icon > <v-icon size="30">mdi-account-circle</v-icon></v-btn>
+    </v-navigation-drawer>
+
     <v-app-bar app>
-        {{name}}
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>{{name}}</v-toolbar-title> 
         <v-spacer></v-spacer>
         <v-btn icon @click="logout()"><v-icon>mdi-logout</v-icon></v-btn>
     </v-app-bar>
+
     <v-main>
-      {{firstlogin}}
+      <!--  -->
     </v-main>
-  </v-app>
+</v-app>
 </template>
 <script>
 export default {
@@ -26,6 +44,7 @@ export default {
         email :'fasdf',
         name : '',
         firstlogin:null,
+        drawer: null
         
     }),
     methods:{
@@ -35,3 +54,10 @@ export default {
     }
 };
 </script>
+<style>
+.nav-pic {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
