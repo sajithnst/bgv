3f8ffdbc0a488a4768a48907349b72ef3937e9c8
sajithsurendran <sajithsurendran007@gmail.com>
2022-10-31 15:26:18 +0530

    the first page for data entry

--- ui/pages/data.vue
+++ ui/pages/data.vue
@@ -8,7 +8,42 @@
     <v-btn icon><v-icon size="32">mdi-arrow-left-bold-outline</v-icon></v-btn>
 </v-app-bar>
 <v-main>
-
+<v-container class="personal">
+  <v-form v-model="isFormvalid" >
+      <h1 class="text-center " > Personal Data</h1><br/>
+      <v-divider></v-divider><br/>
+      <v-col>
+        <v-row>
+            <v-text-field v-model="empid" label="Employee ID" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field v-model="doj" label="Date of Joining" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field v-model="email" label="Personal Email" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field v-model="cmail" label="Official Email" :rules="[rules.required,rules.email]"></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field v-model="mob" label="Mobile Number" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field v-model="aadhaar" label="Aadhaar  Number" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+              <v-text-field v-model="pan" label="PAN  Number" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+              <v-text-field v-model="passport" label="Passport Number" :rules="[rules.required]"></v-text-field>
+        </v-row>
+        <v-row>
+              <v-btn :disabled="!isFormvalid" block large color="teal" :rules="[rules.required]">Save </v-btn>
+        </v-row>
+      </v-col>
+  </v-form>
+  
+</v-container>
 </v-main>
 <h1 class="text-center"></h1>
 </v-app>
@@ -16,6 +51,24 @@
 <script>
 export default{
     name :  "profiledata",
+    async mounted(){
+      this.email = await this.$storage.getUniversal('Email');
+    },
+    data : () =>({
+      email : '',
+      rules:{
+            required: (v) => !!v || "Required",
+            min : (v) =>  v.length > 8 || "Minimun 8 Characters is required",
+            email : (v) => v.match(/\S+@\S+\.\S+/) || "Email format is wrong",
+            name: (v) => v.match(/^[A-Za-z\s]+$/) || "No special Characters in Name"
+        }
+    })
 
 }
 </script>
+<style>
+.personal{
+  width: 30%;
+  margin: 5% auto;
+}
+</style>
