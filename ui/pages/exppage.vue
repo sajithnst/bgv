a0ee34320e9732e51cc7cd529ea178e6915cf725
sajithsurendran <sajithsurendran007@gmail.com>
2023-03-26 12:10:25 +0530

    layout implemented

--- ui/pages/exppage.vue
+++ ui/pages/exppage.vue
@@ -1,29 +1,20 @@
 <template>
-    <v-app>
-        <v-app-bar app color="indigo darken-3"> 
-           <v-btn icon @click="home()" ><v-icon color="white">mdi-home</v-icon></v-btn>
-           <v-spacer></v-spacer>
-        </v-app-bar>
-        <v-main>
             <v-container>
             <exp/>
             </v-container>
-        </v-main>
-    </v-app>
 </template>
 <script>
 export default{
     name : "ugpage",
+    layout:'datacollection',
     async mounted(){
-        this.$vuetify.theme.dark=false;
+    
     },
     data : () =>({
 
     }),
     methods:{
-        async home(){
-            this.$router.push('/')
-        }
+
     }
 }
 </script>
