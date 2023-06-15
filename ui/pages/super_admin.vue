6ab586a7e2ed324d93413a26115f7fb3b1c168b6
J Hari Prasad <jhari00078@gmail.com>
2023-06-15 09:50:17 +0530

    updation

--- ui/pages/super_admin.vue
+++ ui/pages/super_admin.vue
@@ -1,18 +1,29 @@
 <template>
-  <container>
-    <admin/>
-  </container>
+  <v-container  class="signin" >
+      <admin/>
+  </v-container>
 </template>
 <script>
-    export default{
-      name: "admin login",
-      layout:'signinlayout',
-      async mounted(){
-        this.$vuetify.theme.dark=false;
+export default{
+  name: "super_admin",
+  layout:'signinlayout',
+  async mounted(){
+      this.$vuetify.theme.dark=false;
+  },
+  data: () => ({
+
+  }),
+  methods:{
+      async home(){
+          this.$router.push('/');
       },
-      data: () => ({
-
-      }),
-    }
+  }
+}
 </script>
-
+<style>
+.signin{
+  width: 30%;
+  margin: 0% auto;
+  text-align: center;
+}
+</style>
