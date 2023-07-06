fd273643b09f7b1e67c6f57a9fe6b074aa174b8e
J Hari Prasad <77690054+HariPrasad1302@users.noreply.github.com>
2023-07-06 16:20:19 +0530

    updation

--- ui/components/personalupload.vue
+++ ui/components/personalupload.vue
@@ -2,13 +2,13 @@
     <v-container>
         <v-container class="text-center" >
             <v-row>
-              <v-sheet class="text-center" :elevation="6" :height="300" :width="700">
+              <v-card class="text-center" :elevation="6" :height="auto" :width="700">
                 <br>
                 <v-text-title><h2>Personal Data</h2></v-text-title>
                 <br>
                 <v-row>
                     <v-col>
-                        <v-container style="margin-top:10%">
+                        <v-container >
                           <v-container style="width: 100%; ">
                             <v-btn>   
                              <a color="indigo darken-4" style="color: rgb(99, 106, 165);" @click="downloadCSVTemplate">Download Template</a>
@@ -17,13 +17,38 @@
                         </v-container>
                     </v-col>
                     <v-col>
-                        <v-container>
-                            <v-file-input @change="fileselect" style="width:70%; margin:0 auto; margin-top:4%;" label="File input" variant="solo-filled"></v-file-input>
+                        <v-container class="text-center">
+                            <v-file-input @change="fileselect" style="width:70%; margin:0 auto; " label="File input" variant="solo-filled"></v-file-input>
                             <v-btn color="indigo darken-4" style="color:white" @click="upload()">Upload</v-btn><br/><br/>
                           </v-container>
                     </v-col>
                 </v-row>
-              </v-sheet>
+                <v-row>     
+                  <v-container class="text-center">
+                    <h3 v-if="this.data.total_count">Uploaded Details:</h3>
+                  </v-container>               
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-container>
+                      <v-card-subtitle v-if="this.data.total_count">Total count: {{ this.data.total_count }}</v-card-subtitle>
+                    </v-container>
+
+                  </v-col>
+                  <v-col>
+                    <v-container>
+                      <v-card-subtitle v-if="this.data.insert_count">Insert count: {{ this.data.insert_count }}</v-card-subtitle>
+                    </v-container>
+                  </v-col>
+                  <v-col>
+                    <v-container>
+                      <v-card-subtitle v-if="this.data.delete_count">Delete count: {{ this.data.delete_count }}</v-card-subtitle>
+                    </v-container>
+                  </v-col>
+                  
+                </v-row>
+
+              </v-card>
             </v-row>
       
       
@@ -33,6 +58,12 @@
 <script>
 export default {
   name: 'personalupload',
+  data: () => ({
+      total_count: 0,
+      insert_count: {},
+      delete_count: {},
+      data:{}
+    }),
   methods:{
     async fileselect(event){
         this.file=event
@@ -42,7 +73,11 @@ export default {
             formdata.append('csv_file',this.file)
             let furl = "http://127.0.0.1:8000/hr/uploadpersonal"
             let res = await this.$axios.post(furl, formdata);
-            console.log(res.data)
+            this.data = res.data
+            let total_count = res.data.total_count
+            let insert_count = res.insert_count
+            let delete_count = res.delete_count
+            console.log(res.data.total_count)
     }
   }
 }
