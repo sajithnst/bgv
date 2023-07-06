8390f94c2221f99f5332be4d0991ef6044174950
Mohamedsamsul <113825541+Mohamedsamsul@users.noreply.github.com>
2023-07-06 17:05:56 +0530

    upd

--- ui/components/sslcupload.vue
+++ ui/components/sslcupload.vue
@@ -2,53 +2,28 @@
     <v-container>
         <v-container class="text-center" >
             <v-row>
-              <v-card class="text-center" :elevation="6" :height="auto" :width="700">
+              <v-sheet class="text-center" :elevation="6" :height="300" :width="700">
                 <br>
                 <v-text-title><h2>SSLC Data</h2></v-text-title>
                 <br>
                 <v-row>
                     <v-col>
-                        <v-container >
-                          <v-container style="width: 100%; ">
-                            <v-btn>   
-                             <a color="indigo darken-4" style="color: rgb(99, 106, 165);" @click="downloadCSVTemplate">Download Template</a>
-                           </v-btn> 
-                         </v-container>
+                        <v-container style="margin-top:10%">
+                            <v-container style="width: 100%; ">
+                                <v-btn>   
+                                 <a color="indigo darken-4" style="color: rgb(99, 106, 165);" @click="downloadCSVTemplate">Download SSLC Template</a>
+                               </v-btn> 
+                             </v-container>
                         </v-container>
                     </v-col>
                     <v-col>
-                        <v-container class="text-center">
-                            <v-file-input @change="fileselect" style="width:70%; margin:0 auto; " label="File input" variant="solo-filled"></v-file-input>
+                        <v-container>
+                            <v-file-input @change="fileselect" style="width:70%; margin:0 auto; margin-top:4%;" label="File input" variant="solo-filled"></v-file-input>
                             <v-btn color="indigo darken-4" style="color:white" @click="upload()">Upload</v-btn><br/><br/>
                           </v-container>
                     </v-col>
                 </v-row>
-                <v-row>     
-                  <v-container class="text-center">
-                    <h3 v-if="this.data.total_count">Uploaded Details:</h3>
-                  </v-container>               
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-container>
-                      <v-card-subtitle v-if="this.data.total_count">Total count: {{ this.data.total_count }}</v-card-subtitle>
-                    </v-container>
-
-                  </v-col>
-                  <v-col>
-                    <v-container>
-                      <v-card-subtitle v-if="this.data.insert_count">Insert count: {{ this.data.insert_count }}</v-card-subtitle>
-                    </v-container>
-                  </v-col>
-                  <v-col>
-                    <v-container>
-                      <v-card-subtitle v-if="this.data.delete_count">Delete count: {{ this.data.delete_count }}</v-card-subtitle>
-                    </v-container>
-                  </v-col>
-                  
-                </v-row>
-
-              </v-card>
+              </v-sheet>
             </v-row>
       
       
@@ -58,9 +33,6 @@
 <script>
 export default {
   name: 'sslcupload',
-  data: () => ({
-      data:{}
-    }),
   methods:{
     async fileselect(event){
         this.file=event
@@ -70,9 +42,23 @@ export default {
             formdata.append('csv_file',this.file)
             let furl = "http://127.0.0.1:8000/hr/uploadsslc"
             let res = await this.$axios.post(furl, formdata);
-            this.data = res.data
             console.log(res.data)
     }
-  }
+  },
+  methods: {
+    downloadCSVTemplate() {
+      const csvContent ="name,email,sslc_regno,sslc_marks,sslc_passout,sslc_school,sslc_board"
+      const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "sslc.csv";
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    },
+   },
 }
 </script>
