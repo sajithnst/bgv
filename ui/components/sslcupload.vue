bed635a1b433f796c1d93c095d76df904a00e6da
J Hari Prasad <77690054+HariPrasad1302@users.noreply.github.com>
2023-07-07 13:23:09 +0530

    updation

--- ui/components/sslcupload.vue
+++ ui/components/sslcupload.vue
@@ -1,38 +1,71 @@
 <template>
-    <v-container>
-        <v-container class="text-center" >
-            <v-row>
-              <v-sheet class="text-center" :elevation="6" :height="300" :width="700">
-                <br>
-                <v-text-title><h2>SSLC Data</h2></v-text-title>
-                <br>
-                <v-row>
-                    <v-col>
-                        <v-container style="margin-top:10%">
-                            <v-container style="width: 100%; ">
-                                <v-btn>   
-                                 <a color="indigo darken-4" style="color: rgb(99, 106, 165);" @click="downloadCSVTemplate">Download SSLC Template</a>
-                               </v-btn> 
-                             </v-container>
-                        </v-container>
-                    </v-col>
-                    <v-col>
-                        <v-container>
-                            <v-file-input @change="fileselect" style="width:70%; margin:0 auto; margin-top:4%;" label="File input" variant="solo-filled"></v-file-input>
-                            <v-btn color="indigo darken-4" style="color:white" @click="upload()">Upload</v-btn><br/><br/>
-                          </v-container>
-                    </v-col>
-                </v-row>
-              </v-sheet>
-            </v-row>
-      
-      
-          </v-container>
-    </v-container>
+  <v-container>
+      <v-container class="text-center" >
+          <v-row>
+            <v-card class="text-center" :elevation="6" :height="auto" :width="700">
+              <br>
+              <v-text-title><h2>SSLC Data</h2></v-text-title>
+              <br>
+              <v-row>
+                  <v-col>
+                      <v-container >
+                        <v-container style="width: 100%; ">
+                          <v-btn>   
+                           <a color="indigo darken-4" style="color: rgb(99, 106, 165);" @click="downloadCSVTemplate">Download SSLC Template</a>
+                         </v-btn> 
+                       </v-container>
+                      </v-container>
+                  </v-col>
+                  <v-col>
+                    <v-container class="text-center">
+                      <v-file-input @change="fileselect" style="width:70%; margin:0 auto; " label="File input" variant="solo-filled"></v-file-input>
+                      <v-btn   :loading="isLoading" :disabled="isLoading" color="indigo darken-4" style="color:white" @click="upload()">Upload</v-btn>
+                    </v-container>
+                    <v-container>
+                      <v-progress-circular  v-if="isLoading" indeterminate color="indigo darken-4"></v-progress-circular>
+                    </v-container>
+                  </v-col>
+              </v-row>
+              <v-row>     
+                <v-container class="text-center">
+                  <h3 v-if="this.data.total_count">Uploaded Details:</h3>
+                </v-container>               
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-container>
+                    <v-card-subtitle v-if="this.data.total_count">Total count: {{ this.data.total_count }}</v-card-subtitle>
+                  </v-container>
+
+                </v-col>
+                <v-col>
+                  <v-container>
+                    <v-card-subtitle v-if="this.data.insert_count">Insert count: {{ this.data.insert_count }}</v-card-subtitle>
+                  </v-container>
+                </v-col>
+                <v-col>
+                  <v-container>
+                    <v-card-subtitle v-if="this.data.delete_count">Delete count: {{ this.data.delete_count }}</v-card-subtitle>
+                  </v-container>
+                </v-col>
+                
+              </v-row>
+
+            </v-card>
+          </v-row>
+    
+    
+        </v-container>
+  </v-container>
 </template>
 <script>
 export default {
   name: 'sslcupload',
+  data: () => ({
+      data:{},
+      isLoading: false
+
+    }),
   methods:{
     async fileselect(event){
         this.file=event
@@ -43,6 +76,14 @@ export default {
             let furl = "http://127.0.0.1:8000/hr/uploadsslc"
             let res = await this.$axios.post(furl, formdata);
             console.log(res.data)
+
+            this.isLoading = true;
+            // Simulate an asynchronous operation, such as an API call
+            setTimeout(() => {
+              // After the operation is complete, set isLoading to false
+              this.isLoading = false;
+              location.reload();
+            }, 2000);
     },
       downloadCSVTemplate() {
       const csvContent ="name,email,sslc_regno,sslc_marks,sslc_passout,sslc_school,sslc_board"
