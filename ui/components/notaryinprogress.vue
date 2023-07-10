c96837c679f005f09ef5c6e284379e1f74d1f19c
Mohamedsamsul <113825541+Mohamedsamsul@users.noreply.github.com>
2023-07-10 12:53:18 +0530

    updation

--- ui/components/notaryinprogress.vue
+++ ui/components/notaryinprogress.vue
@@ -1,88 +1,104 @@
 <template>
-
   <v-container>
     <v-container v-if="show">
       <v-list>
-      <v-list-item 
-      v-for="request in paginatedRequests" 
-      :key="request.email">
-      <v-list-item-title v-text="request.name"></v-list-item-title>
-      <v-list-item-subtitle v-text="request.email"></v-list-item-subtitle>
-      <v-btn icon @click="view(request.email)"><v-icon color="indigo darken-4">mdi-card-account-details-outline</v-icon></v-btn>
-      <v-btn icon @click="approve(request.email)"><v-icon color="green">mdi-check</v-icon></v-btn>
-      </v-list-item>
-    </v-list>
-    <v-container class="text-center">
-      <v-pagination v-model="currentPage" :total-visible="5" :length="totalPages" @input="changePage"></v-pagination>
-
-    </v-container>
+        <v-list-item
+          v-for="request in paginatedRequests"
+          :key="request.email"
+        >
+          <v-list-item-title v-text="request.name"></v-list-item-title>
+          <v-list-item-subtitle v-text="request.email"></v-list-item-subtitle>
+          <v-btn icon @click="view(request.email)">
+            <v-icon color="indigo darken-4">mdi-card-account-details-outline</v-icon>
+          </v-btn>
+          <v-btn icon @click="approve(request.email)">
+            <v-icon color="green">mdi-check</v-icon>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+      <v-container class="text-center">
+        <v-pagination
+          v-model="currentPage"
+          :total-visible="5"
+          :length="totalPages"
+          @input="changePage"
+        ></v-pagination>
+      </v-container>
     </v-container>
     <v-container v-if="hide">
-      <h2 class="text-center" style ="color:darkblue"> No Profiles</h2>
-      
+      <h2 class="text-center" style="color: darkblue;">No Profiles</h2>
     </v-container>
-  
   </v-container>
-    
 </template>
 
 <script>
 export default {
-  name:"notaryinprogress",
-data() {
-  return {
-    currentPage: 1,
-    paginatedRequests: [], 
-    requests:{},
-    show:false,
-    hide:false,
-  };
-},
-async mounted(){
-  this.$vuetify.theme.dark=false;
-        let url = "http://127.0.0.1:8000/inprogressuser"
-        let res = await this.$axios.get(url)
-        this.paginatedRequests = res.data.list
-        this.requests = res.data.count
+  name: "NotaryInProgress",
 
-        if(this.paginatedRequests==false){
-          this.hide=true
-          this.show=false
-        }
-
-        else{
-          this.hide=false
-          this.show=true
-        }
-},
-computed: {
-  totalPages() {
-    return Math.ceil(this.requests.length / 10);
-  },
-  paginatedRequests() {
-    const startIndex = (this.currentPage - 1) * 10;
-    const endIndex = startIndex + 10;
-    return this.requests.slice(startIndex, endIndex);
-  },
-},
-methods: {
-  changePage(page) {
-    this.currentPage = page;
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 5,
+      profiles: [],
+      requests: {},
+      count: {},
+      notary_email: null,
+      notary_name: null,
+      wallet: 0,
+      show: false,
+      hide: false
+    };
   },
 
-
-  async view(email){
-    this.$storage.setUniversal('user_email', email)
-    this.$router.push("/userprofile")
+  async mounted() {
+    this.$vuetify.theme.dark = false;
+    await this.fetchRequests();
   },
-  async approve(email){
-    let jurl = "http://127.0.0.1:8000/inprogress_verified"
-      let jdata={
-        'email': email,
+
+  computed: {
+    totalPages() {
+      return Math.ceil(this.requests.length / this.pageSize);
+    },
+    paginatedRequests() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.requests.slice(startIndex, endIndex);
+    },
+  },
+
+  methods: {
+    async fetchRequests() {
+      try {
+        const response = await this.$axios.get("http://127.0.0.1:8000/inprogressuser");
+        this.requests = response.data.list;
+        this.show = this.requests.length > 0;
+        this.hide = !this.show;
+      } catch (error) {
+        console.error(error);
       }
-      let jres= await this.$axios.post(jurl,jdata)
-      window.location.reload()
-  }
-},
+    },
+
+    changePage(page) {
+      this.currentPage = page;
+    },
+
+    async view(email) {
+      this.$storage.setUniversal("user_email", email);
+      this.$router.push("/userprofile");
+    },
+
+    async approve(email) {
+      try {
+        const url = "http://127.0.0.1:8000/inprogress_verified";
+        const data = {
+          email: email,
+        };
+        const response = await this.$axios.post(url, data);
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
--- ui/components/notaryusers.vue
+++ ui/components/notaryusers.vue
@@ -1,120 +1,99 @@
 <template>
-  <v-container >
+  <v-container>
     <v-container v-if="show">
       <v-list density="compact">
         <v-list-item
-          v-for="profile in profiles"
+          v-for="profile in paginatedProfiles"
           :key="profile.email"
           :value="profile.email"
           active-color="primary"
         >
           <v-list-item-title v-text="profile.name"></v-list-item-title>
           <v-list-item-subtitle v-text="profile.email"></v-list-item-subtitle>
-          <v-btn icon @click="view(profile.email)"><v-icon color="indigo darken-4">mdi-card-account-details-outline</v-icon></v-btn>
-          <!--<v-btn icon @click="approve(profile.email)"><v-icon color="green">mdi-account-check-outline</v-icon></v-btn>&emsp;&emsp;
-          <v-btn icon @click="deny(profile.email)"><v-icon color="error">mdi-account-remove-outline</v-icon></v-btn>-->
+          <v-btn icon @click="view(profile.email)">
+            <v-icon color="indigo darken-4">mdi-card-account-details-outline</v-icon>
+          </v-btn>
         </v-list-item>
       </v-list>
       <v-container class="text-center">
-      <v-pagination v-model="currentPage" :total-visible="5" :length="totalPages" @input="changePage"></v-pagination>
-
-    </v-container>
-
+        <v-pagination
+          v-model="currentPage"
+          :total-visible="5"
+          :length="totalPages"
+          @input="changePage"
+        ></v-pagination>
+      </v-container>
     </v-container>
     <v-container v-if="hide">
-      <h2 class="text-center" style="color: darkblue;"> No profiles </h2><br /><br/>
+      <h2 class="text-center" style="color: darkblue;">No profiles</h2>
+      <br />
+      <br />
     </v-container>
-
-    </v-container>
+  </v-container>
 </template>
 
 <script>
-export default{
-    name :"notaryusers",
-    
-    async mounted(){
-        this.$vuetify.theme.dark=false;
-        let url = "http://127.0.0.1:8000/pendinguser"
-        let res = await this.$axios.get(url)
-        this.profiles = res.data.list
-        this.count = res.data.count
+export default {
+  name: "NotaryUsers",
 
-        if(this.profiles == false){
-          this.hide = true
-          this.show = false
-        }
-        else{
-          this.show = true
-          this.hide = false
-        }
-    },
-
-    data(){
-      return{
-      currentPage:1,  
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 5,
       profiles: [],
-      requests:{},
-      count:{},
+      requests: {},
+      count: {},
       notary_email: null,
       notary_name: null,
-      wallet:0,
+      wallet: 0,
       show: false,
       hide: false
-
     };
   },
-  computed: {
-     totalPages() {
-       return Math.ceil(this.requests.length / 10);
-     },
-     paginatedRequests() {
-       const startIndex = (this.currentPage - 1) * 10;
-       const endIndex = startIndex + 10;
-       return this.requests.slice(startIndex, endIndex);
-     },
-},
-    methods:{
-      changePage(page) {
-         this.currentPage = page;
+
+  async mounted() {
+    this.$vuetify.theme.dark = false;
+    await this.fetchProfiles();
   },
 
-      async home(){
-        this.$router.push("/");
-      },
-
-      async view(email){
-        let url="http://127.0.0.1:8000/notary/user_lastvisited"
-        let ndata={
-          email: email
-        }
-        let res = await this.$axios.post(url, ndata)
-        this.$storage.setUniversal('user_email',email)
-        this.$storage.setUniversal('hrlogin',0)
-        this.$router.push("/userprofile")
-      },
-     // async approve(email){
-     // let url = "http://127.0.0.1:8000/verify"
-     // let verify ={
-     //  user_email: email,
-     //   notary_email : this.notary_email,
-     //   notary_name: this.notary_name
-     // }
-     //   let res = await this.$axios.post(url,verify)
-     //   console.log(res.data)
-     // },
-     // async deny(email){
-     //   let url = "http://127.0.0.1:8000/verify"
-     //   let verify ={
-     //     user_email: email,
-     //     notary_email : this.notary_email,
-     //     notary_name: this.notary_name,
-     //     status: 'rejected'
-     //   }
-      //  let res = await this.$axios.post(url,verify)
-      //  console.log(res.data)
-      //},
-
+  computed: {
+    totalPages() {
+      return Math.ceil(this.profiles.length / this.pageSize);
+    },
+    paginatedProfiles() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.profiles.slice(startIndex, endIndex);
     }
+  },
 
+  methods: {
+    async fetchProfiles() {
+      try {
+        const response = await this.$axios.get("http://127.0.0.1:8000/pendinguser");
+        this.profiles = response.data.list;
+        this.count = response.data.count;
+        this.show = this.profiles.length > 0;
+        this.hide = !this.show;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    changePage(page) {
+      this.currentPage = page;
+    },
+
+    async view(email) {
+      let url = "http://127.0.0.1:8000/notary/user_lastvisited";
+      let ndata = {
+        email: email
+      };
+      let res = await this.$axios.post(url, ndata);
+      this.$storage.setUniversal("user_email", email);
+      this.$storage.setUniversal("hrlogin", 0);
+      this.$router.push("/userprofile");
+    }
   }
+};
 </script>
